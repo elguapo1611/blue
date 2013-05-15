ab24c1edb2d8d0a04d62df3688d848c49634c3f2
Josh Sharpe <josh.m.sharpe@gmail.com>
2013-05-15 05:44:12 +0000

    troubleshootin

--- lib/capistrano/os.rb
+++ lib/capistrano/os.rb
@@ -17,7 +17,6 @@ module Blue
               sudo "apt-get install -y #{Blue::Os::PACKAGES.join(' ')}"
 
               # Capistrano isn't smart enough to set this up correctly
-              puts application
               path = "/u/apps/#{application}"
               sudo "mkdir -p #{path}"
               sudo "chown #{user} #{path}"
