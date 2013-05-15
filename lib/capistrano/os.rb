2541f6dc27188f03a28f8034f57981e19bcac6ad
Josh Sharpe <josh.m.sharpe@gmail.com>
2013-05-15 05:22:06 +0000

    Add bunches of packages

--- lib/capistrano/os.rb
+++ lib/capistrano/os.rb
@@ -1,8 +1,10 @@
 module Blue
   class Os
     PACKAGES = %w(
-      build-essential
-      zlib1g-dev
+      g++ gcc make libc6-dev patch openssl ca-certificates libreadline6 \
+      libreadline6-dev curl zlib1g zlib1g-dev libssl-dev libyaml-dev \
+      libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev \
+      libgdbm-dev libncurses5-dev automake libtool bison pkg-config libffi-dev
     )
 
     def self.load(capistrano_config)
