c119b353919338fc306856f5bc166c7cd2e54068
Josh Sharpe <josh.m.sharpe@gmail.com>
2013-05-16 15:50:20 +0000

    hook into db:migrate during deploy

--- lib/capistrano/rails.rb
+++ lib/capistrano/rails.rb
@@ -3,16 +3,16 @@ module Blue
     def self.load(capistrano_config)
       capistrano_config.load do
 
-        namespace :blue do
-          namespace :db do
-            desc "Install required gems"
-            task :migrate do
-              run "cd #{Blue.current_release_dir} && RAILS_ENV=#{Bundle.env} bundle exec rake db:migrate"
-            end
-          end
-        end
+        # namespace :blue do
+        #   namespace :db do
+        #     desc "Install required gems"
+        #     task :migrate do
+        #       run "cd #{Blue.current_release_dir} && RAILS_ENV=#{Bundle.env} bundle exec rake db:migrate"
+        #     end
+        #   end
+        # end
 
-        after 'blue:apply_manifest', 'blue:db:migrate'
+        after 'blue:apply_manifest', 'deploy:migrations'
       end
     end
   end
