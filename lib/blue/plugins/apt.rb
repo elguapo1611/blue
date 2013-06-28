d328273f994df3a96d5430b1046c04c5872aea5a
Josh Sharpe <josh.m.sharpe@gmail.com>
2013-06-28 21:15:59 +0000

    fix template path

--- lib/blue/plugins/apt.rb
+++ lib/blue/plugins/apt.rb
@@ -5,7 +5,7 @@ module Blue
       file "/etc/apt/sources.list.d/apt.list",
         :ensure  => :present,
         :mode    => '744',
-        :content => template(File.join(File.dirname(__FILE__), '..', '..', 'templates', 'apt.list'), binding)
+        :content => template(File.join(File.dirname(__FILE__), '..', '..', '..', 'templates', 'apt.list'), binding)
     end
 
     def self.included(klass)
