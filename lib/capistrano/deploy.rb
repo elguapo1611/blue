65b5bb50cfb31c2986388e57436314d02e599cc9
Josh Sharpe <josh.m.sharpe@gmail.com>
2013-06-29 20:43:00 +0000

    remove tests

--- lib/capistrano/deploy.rb
+++ lib/capistrano/deploy.rb
@@ -7,9 +7,6 @@ module Blue
           desc 'Apply the Blue manifest for this application'
           task :apply_manifest, :except => { :no_release => true } do
             current_host = capture("echo $CAPISTRANO:HOST$").strip.gsub('.', '_')
-cmd = "cd ~/ && pwd"
-run "sudo su - -c '#{cmd}'"
-run "#{sudo} #{cmd}"
             run "sudo su - -c 'cd #{latest_release} && RAILS_ROOT=#{latest_release} RAILS_ENV=#{Blue.env} shadow_puppet #{latest_release}/config/blue/boxes/#{Blue.env}/#{current_host}.rb'"
           end
 
