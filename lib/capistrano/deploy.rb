e353d5b1a3f714fbec6b21d1565080564f601dac
Josh Sharpe <josh.m.sharpe@gmail.com>
2013-07-01 05:20:48 +0000

    try this

--- lib/capistrano/deploy.rb
+++ lib/capistrano/deploy.rb
@@ -7,7 +7,8 @@ module Blue
           desc 'Apply the Blue manifest for this application'
           task :apply, :except => { :no_release => true } do
             # current_host = capture("echo $CAPISTRANO:HOST$").strip.gsub('.', '_')
-            run "sudo su - -c 'cd #{latest_release} && RAILS_ROOT=#{latest_release} RAILS_ENV=#{Blue.env} bundle exec shadow_puppet #{latest_release}/config/blue/#{Blue.env}/$(hostname).rb'"
+            current_host = capture("echo $(hostname)").gsub('-', '_')
+            run "sudo su - -c 'cd #{latest_release} && RAILS_ROOT=#{latest_release} RAILS_ENV=#{Blue.env} bundle exec shadow_puppet #{latest_release}/config/blue/#{Blue.env}/#{current_host}.rb'"
           end
 
           task :verify_db do
