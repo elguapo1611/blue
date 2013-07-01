b11a9b08ab4a969f44c16b771dea6d507262e5f6
Josh Sharpe <josh.m.sharpe@gmail.com>
2013-07-01 05:01:31 +0000

    apply manifest to all servers

--- lib/capistrano/deploy.rb
+++ lib/capistrano/deploy.rb
@@ -5,7 +5,7 @@ module Blue
 
         namespace :blue do
           desc 'Apply the Blue manifest for this application'
-          task :apply, :roles => :os, :except => { :no_release => true } do
+          task :apply, :except => { :no_release => true } do
             current_host = capture("echo $CAPISTRANO:HOST$").strip.gsub('.', '_')
             run "sudo su - -c 'cd #{latest_release} && RAILS_ROOT=#{latest_release} RAILS_ENV=#{Blue.env} bundle exec shadow_puppet #{latest_release}/config/blue/#{Blue.env}/#{current_host}.rb'"
           end
